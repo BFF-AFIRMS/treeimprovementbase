1a55c83418901f493b890841d7a3ed8ad8f4c586
Katherine Eaton <ktmeaton@gmail.com>
2025-12-16 17:48:11 -0700

    patches: trait acronym and time ontology year

---
+++ patches/00001/AddTimeOntologyYear.pm
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+AddTimeOntologyYear.pm
+
+=head1 SYNOPSIS
+
+mx-run AddTimeOntologyYear [options] [-F]
+
+this is a subclass of L<CXGN::Metadata::Dbpatch>
+see the perldoc of parent class for more details.
+
+=head1 DESCRIPTION
+
+This patch:
+ - Adds new cvterm for time ontology: year
+
+=head1 AUTHOR
+
+Katherine Eaton
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2025 University of Alberta
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+package AddTimeOntologyYear;
+
+use Moose;
+use Bio::Chado::Schema;
+use SGN::Model::Cvterm;
+extends 'CXGN::Metadata::Dbpatch';
+
+has '+description' => ( default => <<'' );
+Adds new cvterms for time ontology age in years: year 1-100.
+
+sub patch {
+    my $self=shift;
+
+    print STDOUT "Executing the patch:\n " .   $self->name . ".\n\nDescription:\n  ".  $self->description . ".\n\nExecuted by:\n " .  $self->username . " .";
+
+    print STDOUT "\nChecking if this db_patch was executed before or if previous db_patches have been executed.\n";
+
+    print STDOUT "\nExecuting the SQL commands.\n";
+    my $schema = Bio::Chado::Schema->connect( sub { $self->dbh->clone } );
+
+    print STDERR "INSERTING CV TERMS...\n";
+
+    my $age_in_years_rs = $schema->resultset("Cv::Cvterm")->create_with({ name => "age in years", definition=> "Years", cv => 'cxgn_time_ontology' });
+    my $age_in_years_cvterm_id = $age_in_years_rs->cvterm_id();
+    my $is_a_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'is_a', 'relationship')->cvterm_id();
+
+    foreach my $year (1..100) {
+        my $year_rs = $schema->resultset("Cv::Cvterm")->create_with({ name => "year $year", cv => 'cxgn_time_ontology' });
+        my $year_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, "year $year", 'cxgn_time_ontology')->cvterm_id();
+        $schema->resultset("Cv::CvtermRelationship")->create({
+            type_id    => $is_a_cvterm_id,
+            subject_id => $year_rs->cvterm_id(),
+            object_id  => $age_in_years_cvterm_id
+        });
+    }
+
+    print "You're done!\n";
+}
+
+####
+1; #
+####
