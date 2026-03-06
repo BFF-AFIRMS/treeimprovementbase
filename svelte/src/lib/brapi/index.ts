d94705faf8a14db34302d52c2edfb1ec72d3048e
Katherine Eaton <ktmeaton@gmail.com>
2026-03-06 11:49:21 -0700

    svelte: add env template

--- svelte/src/lib/brapi/index.ts
+++ svelte/src/lib/brapi/index.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_BRAPI_URL } from '$env/static/public';
-export const brapi_url = PUBLIC_BRAPI_URL;
+import { PUBLIC_BREEDBASE_URL } from '$env/static/public';
+export const brapi_url = PUBLIC_BREEDBASE_URL + "/brapi/v2";
 export {Pagination} from './pagination.ts';
 export *  as v2 from './v2';
