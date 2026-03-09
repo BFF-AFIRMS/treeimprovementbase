8cf1446abe5032ce11caac89f9eed5631f5d0590
Katherine Eaton <ktmeaton@gmail.com>
2026-03-09 09:56:48 -0600

    svelte: add navbar to components

--- svelte/src/lib/components/app/app.svelte
+++ svelte/src/lib/components/app/app.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	let { children } = $props();
+    import Navbar from "$lib/components/app/navbar.svelte";
 </script>
 
 <div style="text-align: center">
     <!--  style="min-width: var(--breakpoint-xs); max-width: var(--breakpoint-lg)" -->
       <!-- inline-grid -->
     <div class="border border-green-800 rounded-xl p-4 overflow-auto w-full h-screen"  style="min-width: var(--breakpoint-xs)">
+        <Navbar/>
         {@render children()}
     </div>
 </div>
