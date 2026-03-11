9912df1aef1a7b13ac2899d6c25f0c24aa34f5b1
Katherine Eaton <ktmeaton@gmail.com>
2026-03-11 15:00:43 -0600

    lib: fix files permissions issue in tmp

--- lib/SGN/Controller/Svelte.pm
+++ lib/SGN/Controller/Svelte.pm
@@ -4,10 +4,16 @@ use Moose;
 
 BEGIN { extends 'Catalyst::Controller' };
 
-sub people_search : Path('/svelte/breeders/manage_programs') Args(0) {
+sub breeding_programs : Path('/svelte/breeders/manage_programs') Args(0) {
     my $self = shift;
     my $c = shift;
     $c->stash->{template} = '/svelte/breeders/manage_programs.html';
+}
+
+sub people_search : Path('/svelte/search/organisms') Args(0) {
+    my $self = shift;
+    my $c = shift;
+    $c->stash->{template} = '/svelte/search/organisms.html';
 }
 
 1;
