407a8274ad86dcb2cb4e9363132275d20daf1b2b
Katherine Eaton <ktmeaton@gmail.com>
2026-03-26 18:10:08 -0600

    svelte: add temporary fallback

--- svelte/src/routes/stock/[id]/+page.ts
+++ svelte/src/routes/stock/[id]/+page.ts
@@ -2,9 +2,11 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { detail  } from '$lib/brapi/v2/germplasm';
 
-export const load: PageLoad = async ({ fetch, params }) => {
+export const prerender = false;
 
-    let result = await detail({germplasmDbId: Number(params.slug)});
+export const load: PageLoad = async ({ params }) => {
+
+    let result = await detail({germplasmDbId: Number(params.id)});
     if (result.data && result.data.result){
         return {result: result.data.result}
     } else {
